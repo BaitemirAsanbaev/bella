8b0cd24ac636bbc38c7f1323907787cbccbc4838
Baitemir <ferrari170604@gmail.com>
2021-06-16 00:30:48 +0600

    setted drawer items

--- src/containers/Layout/Drawer/Drawer.js
+++ src/containers/Layout/Drawer/Drawer.js
@@ -1,4 +1,4 @@
-import Nav from "../../../components/Nav/Nav"
+import { NavLink } from "react-router-dom";
 import Backdrop from "../Backdrop/Backdrop";
 import classes from './Drawer.module.scss';
 
@@ -12,7 +12,13 @@ const Drawer = ({ open, closeDrawer }) => {
   return (<div className={classes.Drawer}>
     <Backdrop show={open} click={closeDrawer} />
     <div className={drawerClasses.join(" ")}>
-      <Nav click={closeDrawer} />
+      <ul>
+        <li><NavLink to="/products" activeStyle={{color: '#2B2A2A'}} exact={true}>Товары</NavLink></li>
+        <li><NavLink to="/about" activeStyle={{color: '#2B2A2A'}}>О нас</NavLink></li>
+        <li><NavLink to="/news" activeStyle={{color: '#2B2A2A'}}>Новости</NavLink></li>
+        <li><NavLink to="/delivery" activeStyle={{color: '#2B2A2A'}}>Доставка</NavLink></li>
+        <li><NavLink to="/contacts" activeStyle={{color: '#2B2A2A'}}>Контакты</NavLink></li>
+      </ul>
     </div>
   </div>);
 }
