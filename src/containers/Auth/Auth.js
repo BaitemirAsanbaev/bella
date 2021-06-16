5f28c044e9889c1a102276424dca2e756ad26e68
Akyl <akylbekmuratbekuulu@gmail.com>
2021-06-16 16:03:41 +0600

    auth

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,42 +1,56 @@
 import classes from './Auth.module.css'
 import logo from '../../assets/img/Bella.svg'
 import { Link } from 'react-router-dom';
-import { useState } from 'react'
+import React from 'react';
+import { useState, useEffect } from 'react';
 
 
 const Auth = () => {
+  const [phone, setPhone] = useState('')
+  const [phoneDirty, setPhoneDirty] = useState(false)
+  const [phoneError, setPhoneError] = useState('Ввдите номер телефона')
+  const [formValid, setFormValid] = useState(false)
 
-     
-     const [tel, setTel] = useState("");
-     const [error, setError] = useState(false);
-     function checkError(){
-          if(tel === ""){
-               setError(true);
-          }
-     }
+  useEffect( () =>{
+      if(phoneError){
+       setFormValid(false)
+      }else{
+        setFormValid(true)
+      }
+  }, [phoneError])
 
-     const errorMessage = <span>Введите ноиер телевона</span>
+  const phoneHandler = (e) => {
+    setPhone(e.target.value)
+    const re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im
+    if (!re.test(Number(e.target.value))) {
+      setPhoneError('Введите ваш номер')
+    } else {
+      setPhoneError("")
+    }
+  }
 
-    return ( 
-        <div className={classes.modal}>
-             <div className={classes.img}><img alt="logo" src={logo} /></div>
-             <form className={classes.form}>
-                <p>ВХОД</p>
-                <input
-                required
-                className={classes.input}
-                placeholder="Введите номер телефона"
-                onInput={(event)=>setTel(event.target.value)}/>
-                <button className={classes.button} onClick={checkError}>ВОЙТИ</button>
-                {error} ? {errorMessage} : null
-              <Link to="/registration"><p className={classes.p}>Зарегистрироваться</p></Link>  
-             
-             </form>
+  const blurHandler = (e) => {
+    switch (e.target.name) {
+      case 'phone':
+        setPhoneDirty(true)
+        break
+      default:
+        break
+    }
+  }
 
-         
-            
-        </div>
-     );
+  return (
+    <div className={classes.modal}>
+      <div className={classes.img}><img alt="logo" src={logo} /></div>
+      <form className={classes.form}>
+        <p>ВХОД</p>
+        <input onChange={e => phoneHandler(e)} value={phone} className={classes.input} placeholder="Введите номер телефона" name='phone' onBlur={e => blurHandler(e)} />
+        <button disabled={!formValid} className={classes.button}>ВОЙТИ</button>
+        <Link to="/registration"><p className={classes.p}>Зарегистрироваться</p></Link>
+        {(phoneDirty && phoneError) && <div className={classes.error} style={{ color: 'red'} }>{phoneError}</div>}
+      </form> 
+    </div>
+  );
 }
- 
+
 export default Auth;
