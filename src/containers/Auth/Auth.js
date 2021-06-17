c6ac0256392df858a7f0720de390011c9bed08ef
Baitemir <ferrari170604@gmail.com>
2021-06-17 16:06:07 +0600

    Revert "Merge branch 'main' of https://github.com/BaitemirAsanbaev/bella-2 into Auth"
    This reverts commit ab8d23320ecb70cf75672cc398e49d0b923a7fca, reversing
    changes made to 86e7bf24d36372d71762e03e636a026e016413ea.

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,36 +1,17 @@
 import classes from './Auth.module.css'
 import logo from '../../assets/img/Bella.svg'
 import { Link } from 'react-router-dom';
-import { useState } from 'react'
-
 
 const Auth = () => {
-
-     
-     const [tel, setTel] = useState("");
-     const [error, setError] = useState(false);
-     function checkError(){
-          if(tel === ""){
-               setError(true);
-          }
-     }
-
-     const errorMessage = <span>Введите ноиер телевона</span>
-
     return ( 
         <div className={classes.modal}>
              <div className={classes.img}><img alt="logo" src={logo} /></div>
              <form className={classes.form}>
                 <p>ВХОД</p>
-                <input
-                required
-                className={classes.input}
-                placeholder="Введите номер телефона"
-                onInput={(event)=>setTel(event.target.value)}/>
-                <button className={classes.button} onClick={checkError}>ВОЙТИ</button>
-                {error} ? {errorMessage} : null
+                <input  className={classes.input} placeholder="Введите номер телефона"/>
+                <button className={classes.button}>ВОЙТИ</button>
               <Link to="/registration"><p className={classes.p}>Зарегистрироваться</p></Link>  
-             
+
              </form>
 
          
