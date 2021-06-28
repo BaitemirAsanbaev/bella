7f32631c6d465390fd030171038154b9958d3b1c
Akyl <akylbekmuratbekuulu@gmail.com>
2021-06-28 11:23:36 +0600

    profile  and slider

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,43 @@
+import classes from "./Profile.module.css"
+import profile from "../../assets/img/Profile.png"
+import Bella from "../../assets/img/Bella.png"
+
+const Profile = () => {
+    return ( 
+        <div>
+          <div className={classes.container}>
+            <div className={classes.wrapp}> 
+                
+                <form className={classes.form}>
+        <h2 className={classes.registr}>Профиль</h2>
+            <label className={classes.label}>Ваше имя </label>
+            <input className={classes.input} placeholder="Имя" />
+            <label className={classes.label}>Ваша фамилия </label>
+            <input className={classes.input} placeholder="Фамилия" />
+            <label className={classes.label}>Номер телефона</label>
+            <input className={classes.input} placeholder=" Номер телефона" />
+        <button className={classes.button}>ВОЙТИ</button>
+      </form>
+
+      <form className={classes.form}>
+        <h2 className={classes.registr}>Адрес доставки</h2>
+            <label className={classes.label}>Ваше имя </label>
+            <input className={classes.input} placeholder="Имя" />
+            <label className={classes.label}>Ваша фамилия </label>
+            <input className={classes.input} placeholder="Фамилия" />
+        <button className={classes.button}>ВОЙТИ</button>
+      </form>
+            </div>
+
+            <div className={classes.img}>  
+                 <div className={classes.bell}><img src={Bella} alt="Bell"/></div>
+                <img src={profile} alt="profile"/>
+                
+            </div>
+          </div>
+
+        </div>
+     );
+}
+ 
+export default Profile;
---
+++ src/components/Slick.js/Slider.js
@@ -0,0 +1,54 @@
+
+import img from "../../../src/assets/img/slick.png"
+import classes from "./Slick.module.css"
+import React, { Component } from 'react';
+import Slider from 'infinite-react-carousel';
+
+export default class CustomSlider extends Component {
+  render() {
+    const settings =  {
+      autoplay: true,
+      autoplaySpeed: 100000,
+      dots: true,
+      arrows:false,
+      appendDots: dots => (
+        <div
+          style={{
+            position:"absolute",
+            top:"90%",
+            zIndex:"3"
+          }}
+        >
+          <ul style={{ margin: "0px" }} className={classes.ul}> {dots} </ul>
+        </div>
+      ),
+    };
+    return (
+      <div className={classes.slick}>  
+        <Slider { ...settings }>
+           
+        <div>
+             <img src={img} alt="phot"/>
+             
+          </div>
+          <div>
+           <img src={img} alt="phot"/>
+          </div>
+           <div>
+          <img src={img} alt="phot"/>
+          </div>
+           <div>
+           <img src={img} alt="phot"/>
+          </div>
+          <div>
+          <img src={img} alt="phot"/>
+          </div>
+          <div>
+          <img src={img} alt="phot"/>
+          </div>
+          
+        </Slider>
+      </div>
+    );
+  }
+}
